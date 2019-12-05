65f4af1f0216c3372549f6dd6c2d16cddbc4ebc0
msmeta <36460181+msmeta@users.noreply.github.com>
2019-12-05 13:57:37 +0100

    Add files via upload

--- MC_1_4.m
+++ MC_1_4.m
@@ -12,7 +12,7 @@ L=300;
 N1=1000;
 N2=1000;
 
-put_0 = @(x) exp(-r*T2)*max(K-x,0);
+put_T0 = @(x) exp(-r*T2)*max(K-x,0);
 call_T2 = @(x) exp(-r*(T1-T2))*max(x-L,0);
 
 % simulate values of the underlying at time T2
@@ -23,9 +23,9 @@ S_T2 = S0*exp((r-sigma^2/2)*T2+sqrt(T2)*sigma*Z1);
 Z2 = randn(N1,N2);
 call_prices_T2=[];
 for i=1:N1
-    S_T1 = S_T2(i)*exp((r-sigma^2/2)*3+sqrt(3)*sigma*Z2(i,:)');
+    S_T1 = S_T2(i)*exp((r-sigma^2/2)*(T1-T2)+sqrt(T1-T2)*sigma*Z2(i,:)');
     call_prices_T2=[call_prices_T2; mean(call_T2(S_T1))];
 end
 
 % put prices at time 0
-put_prices_0 = put_0(call_prices_T2);
+put_prices_T0 = put_T0(call_prices_T2);
